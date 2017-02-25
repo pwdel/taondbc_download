7b72ffd9aa02485edc145c90295ec65a26fa767c
dreyco676 <dreyco676@gmail.com>
2017-02-25 14:07:49 -0600

    fixed quotes

--- fetch_ndbc_realtime.py
+++ fetch_ndbc_realtime.py
@@ -4,15 +4,15 @@ from bs4 import BeautifulSoup
 
 
 # header fields are links and we should igonore them
-header_fields = ["Name", "Last modified", "Size", "Description", "Parent Directory"]
+header_fields = ['Name', 'Last modified', 'Size', 'Description', 'Parent Directory']
 
 # page with all the links for files
-realtime_url = "http://www.ndbc.noaa.gov/data/realtime2/"
+realtime_url = 'http://www.ndbc.noaa.gov/data/realtime2/'
 
 # read the website html and find the links to the files
 r = req.get(realtime_url)
 soup = BeautifulSoup(r.text, 'lxml')
-links = soup.find_all("a")
+links = soup.find_all('a')
 
 # skip the header links
 for link in links:
@@ -20,7 +20,7 @@ for link in links:
         continue
 
     # get the link to the file
-    base_href = link.get("href")
+    base_href = link.get('href')
     full_url = realtime_url + base_href
     link_data = req.get(full_url)
 
