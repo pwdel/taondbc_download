0481c0a5aa070f3cb11c02e366b616a0f03502b2
dreyco676 <dreyco676@gmail.com>
2017-02-25 14:03:57 -0600

    added gzip
    added comments & gzip of files

--- fetch_ndbc_realtime.py
+++ fetch_ndbc_realtime.py
@@ -2,23 +2,29 @@
 import requests as req
 from bs4 import BeautifulSoup
 
-header_fields=["Name","Last modified","Size","Description","Parent Directory"]
 
-realtime_url="http://www.ndbc.noaa.gov/data/realtime2/"
-r=req.get(realtime_url)
-soup=BeautifulSoup(r.text, 'lxml')
-links=soup.find_all("a")
+# header fields are links and we should igonore them
+header_fields = ["Name", "Last modified", "Size", "Description", "Parent Directory"]
 
+# page with all the links for files
+realtime_url = "http://www.ndbc.noaa.gov/data/realtime2/"
+
+# read the website html and find the links to the files
+r = req.get(realtime_url)
+soup = BeautifulSoup(r.text, 'lxml')
+links = soup.find_all("a")
+
+# skip the header links
 for link in links:
     if link.text in header_fields: 
         continue
 
-    base_href=link.get("href")
+    # get the link to the file
+    base_href = link.get("href")
     full_url = realtime_url + base_href
-    link_data=req.get(full_url)
+    link_data = req.get(full_url)
 
-    ofname=base_href
-    ofile=open(ofname,'w')
-    ofile.write(link_data.text)
-    ofile.close()
-
+    # save the file
+    file_name = base_href + '.gz'
+    with gzip.open(file_name, 'wb') as f:
+        f.write(link_data.text)
