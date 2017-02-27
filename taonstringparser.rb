0a53db800e1790418783c89ed4c9d168584c72c6
Patrick Delaney <iotsoftwr3000@gmail.com>
2017-02-26 18:38:01 -0600

    Adding initial data.csv file (non-full links)

--- taonstringparser.rb
+++ taonstringparser.rb
@@ -15,16 +15,26 @@ end
 
 @bouy_blocks = Array.new
 page.search('pre a').each do |block|
+page.search('pre a').map { |link| link['href'] }.each do |links|
   # puts block.text
-  id_hash = { :Bouyid => block.text }
+  id_hash = { :Bouyid => block.text, :Bouyweb => links }
   # puts hash
   @bouy_blocks.push(id_hash)
 end
-# puts @buoy_blocks1
-puts @bouy_blocks
-
-=begin
-pre_blocks = page.search('pre a').map { |link| link['href'] }.each do ||
-  puts block
 end
-=end
+
+#puts @bouy_blocks
+
+# Get headers, the keys for each hash key-value pair
+headers = @bouy_blocks[0].keys
+
+# Open CSV File, Write Headers Into First Row, Populate Data With Hash
+CSV.open("data.csv", "wb", headers: :first_row) do |csv|
+  # write the headers to the CSV
+  csv << headers
+  # iterate over the hashes
+  @bouy_blocks.each do |bz|
+  # extract values and write to CSV file
+  csv << bz.values_at(*headers)
+end
+end
